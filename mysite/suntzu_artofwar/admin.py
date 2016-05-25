0e673f92f1f8b4656edc8ba25fb529cfee2da04c
alenush <alenusch93@yandex.ru>
2016-05-25 11:36:30 +0300

    Db is ready

--- mysite/suntzu_artofwar/admin.py
+++ mysite/suntzu_artofwar/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Chapters, ChaptersTranslation, Comments
+
+admin.site.register(Chapters)
+admin.site.register(ChaptersTranslation)
+admin.site.register(Comments)
--- mysite/suntzu_artofwar/models.py
+++ mysite/suntzu_artofwar/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Chapters(models.Model):
+
+    chapter = models.PositiveIntegerField(verbose_name='chapter')
+    sub_chapter = models.PositiveIntegerField(verbose_name='sub_chapter')
+    ch_content = models.CharField(max_length=1000, verbose_name='sub_ch_content')
+
+class ChaptersTranslation(models.Model):
+
+    chapter = models.PositiveIntegerField(verbose_name='chapter')
+    sub_chapters = models.CharField(max_length=10, verbose_name='sub_chapters')
+    translator = models.CharField(max_length=200, verbose_name='translator')
+    translation = models.CharField(max_length=2000, verbose_name='translations')
+
+class Comments(models.Model):
+
+    chapter = models.PositiveIntegerField(verbose_name='chapter')
+    sub_chapters = models.PositiveIntegerField(verbose_name='sub_chapter')
+    commentator= models.CharField(max_length=200, verbose_name='commentator')
+    comment = models.CharField(max_length=2000, verbose_name='comment')
+    com_translator = models.CharField(max_length=200, verbose_name='trans_commentator', blank=True)
+    com_translation = models.CharField(max_length=2000, verbose_name='com_translation', blank=True)
